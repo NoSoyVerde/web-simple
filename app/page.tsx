e54c46880064efac032ec28cc5b2abee0e9c8f1f
NoSoyVerde <hectorkotor@gmail.com>
2025-10-07 14:42:51 +0200

    Añadidos detalles sobre space

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,11 @@ export default function Home() {
       <>
         <Header />
         <section id='conceptos'>
-          <h1 className="text-2xl font-extrabold text-slate-100 "></h1>
+          <h1 className="text-2xl font-extrabold text-slate-100 p-3 mx-auto mb-6 overflow-clip">Conceptos Fundamentales</h1>
+          <ul className="m-3 md:m-8 flex flex-col md:flex-row space-y-0 md:space-x-6">
+            
+
+          </ul>
         </section>
       </>
     );
